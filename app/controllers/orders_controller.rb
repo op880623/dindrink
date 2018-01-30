f2c0d658c7c161904487b3b479018b83b5cf80e0
op880623 <op880623@gmail.com>
2018-01-30 17:36:53 +0800

    add shop info while create order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,18 @@
 class OrdersController < ApplicationController
+  require 'JSON'
 
-  def new
+  def index
+    @shops = Shop.all
   end
 
   def create
-    @order = Order.create()
+    @order = Order.create(content: {shop: params[:shopid]}.to_json)
     redirect_to order_path(@order.hashid)
   end
 
   def show
     @order = Order.find(params[:hashid])
+    @shop = Shop.find(JSON.load(@order.content)["shop"])
   end
 
   def update
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "orders#new"
-  post "/", to: "orders#create"
+  root "orders#index"
+  post "/:shopid", to: "orders#create", as: :create_order
   get "/:hashid", to: "orders#show", as: :order
   match "/:hashid", to: "orders#update", via: [:patch, :put]
 end
