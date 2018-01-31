225870697e537df6b716275e023743aa4f4ba885
op880623 <op880623@gmail.com>
2018-01-31 12:25:02 +0800

    add price column and be able to add drink on show page

---
+++ app/controllers/drinks_controller.rb
@@ -0,0 +1,12 @@
+class DrinksController < ApplicationController
+
+  def create
+    @drink = Drink.new(params.require(:drink).permit(:name, :price))
+    @drink.shop_id = params[:shop_id]
+    if !@drink.save
+      puts @drink.errors.full_messages.to_sentence
+    end
+    redirect_back fallback_location: root_path
+  end
+
+end
