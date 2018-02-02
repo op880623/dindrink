5e14d726f9e1fb4b46cec48e5e46ede532909bb7
op880623 <op880623@gmail.com>
2018-02-02 15:32:00 +0800

    change price dynamically

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,3 +1,8 @@
+var change_price = function() {
+  drink_id = document.getElementById("drink_id").value;
+  document.getElementById("price").innerHTML = price[drink_id];
+};
+
 var price;
 
 function get_drinks_prices(shop_id) {
