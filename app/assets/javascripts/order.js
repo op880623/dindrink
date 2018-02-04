39ccc2782dce4869eb48c5c1cf177e7e9c891668
op880623 <op880623@gmail.com>
2018-02-04 15:35:00 +0800

    able to display options

--- app/assets/javascripts/order.js
+++ app/assets/javascripts/order.js
@@ -1,7 +1,24 @@
 function display_info() {
   drink = info[document.getElementById("drink_id").value];
   document.getElementById("price").innerHTML = drink['price'];
-  document.getElementById("option").innerHTML = drink['option'];
+
+  td_tag = document.getElementById("option");
+  while (td_tag.childNodes.length > 0) {
+    td_tag.removeChild(td_tag.childNodes[0]);
+  }
+
+  option_content = JSON.parse(drink['option']);
+  for (key in option_content) {
+    select_tag = document.createElement("select");
+    td_tag.appendChild(select_tag);
+    type = option_content[key];
+    for (option in type) {
+      option_tag = document.createElement("option");
+      option_tag.setAttribute("value", type[option])
+      option_tag.innerHTML = type[option];
+      select_tag.appendChild(option_tag);
+    }
+  }
 }
 
 var info;
