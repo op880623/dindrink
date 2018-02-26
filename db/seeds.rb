648c68eb110b8b5f05633e20ef9d6f08f0421792
op880623 <op880623@gmail.com>
2018-02-26 17:04:42 +0800

    add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'yaml'
+
+Shop.destroy_all
+Drink.destroy_all
+
+shops = YAML.load_file('db/data.txt')
+shops.each do |shop|
+  s = Shop.create(name: shop[:name],
+    tel: shop[:tel],
+    address: shop[:address],
+    menu: Rails.root.join(shop[:menu]).open,
+    custom: false
+  )
+  shop[:drinks].each do |drink, price|
+    Drink.create(name: drink,
+      price: price,
+      shop: s
+    )
+  end
+end
+puts "create seed data"
